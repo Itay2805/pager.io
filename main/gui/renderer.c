17a6dc20ec9fbf97b836325ba85ee7fef1c7b096
Itay Almog <itay2828@gmail.com>
2025-04-10 21:44:02 +0300

    The clear count on exit takes care of that actually

--- main/gui/renderer.c
+++ main/gui/renderer.c
@@ -7,18 +7,13 @@
 
 #include <drivers/lcd_panel.h>
 
-static volatile bool m_renderer_waiting = false;
-
 /**
  * Called wehenever the frame buffer DMA is finished, this lets us know we can now start working on
  * processing the next frame
  */
 static bool on_frame_buf_complete(esp_lcd_panel_handle_t panel, const esp_lcd_rgb_panel_event_data_t* edata, void* user_ctx) {
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    if (m_renderer_waiting) {
-        m_renderer_waiting = false;
-        vTaskNotifyGiveFromISR(user_ctx, &xHigherPriorityTaskWoken);
-    }
+    vTaskNotifyGiveFromISR(user_ctx, &xHigherPriorityTaskWoken);
     return xHigherPriorityTaskWoken;
 }
 
@@ -48,12 +43,6 @@ static void renderer_task(void* arg) {
 
     // and start the render loop
     for (;;) {
-        // mark that we are waiting for the framebuffer to complete rendering
-        // NOTE: this synchronization is meant to keep us in sync with the frame
-        //       rendering and not get offset and start tearing if we took too
-        //       long to render for whatever reason
-        m_renderer_waiting = true;
-
         // wait for the last framebuffer transmission to finish
         // before we are trying to draw the next one
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
